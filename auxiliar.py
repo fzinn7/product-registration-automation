f754fdec3a9bff68de0eb7dc3f4fb2a0752f7d10
Fabricio Ramos Oliveira <161607854+fzinn7@users.noreply.github.com>
2026-04-08 16:12:24 -0300

    auxiliar.py

---
+++ auxiliar.py
@@ -0,0 +1,11 @@
+# Código criado apenas para encontrar as posições corretas para rodar o código perfeitamente
+
+import pyautogui
+import time
+
+time.sleep(5)
+posição = pyautogui.position()
+print (posição)
+
+
+
